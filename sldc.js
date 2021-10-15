2089e612295f82df418fb804968656232de0dc26
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-15 16:59:44 -0500

    reorganized steps & created new variables for SDLC

--- sldc.js
+++ sldc.js
@@ -18,3 +18,9 @@
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
+let analysisReq   = "Eliticing business/project requirements in layman's terms"
+let planning      = "Parsing user stories and creating specific assessment criteria"
+let design        = "This step could refer to the architect's overall design or big picture of the project or it could refer to the front end UI/UX code"
+let implement     = "This is where all of the code is put together into production, including backend code."
+let testIntegrate = "User acceptance testing by various project stakeholders and implementation into the organization at large. This step would also include onboarding."
+let maintain      = "Once the software is completed it will always require regular updates and bug fixes."
