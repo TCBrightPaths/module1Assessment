99470849c3f256d2f3c3baaa61b086d4d51c8182
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-16 17:29:08 -0500

    added interface

---
+++ password.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
+	
+	// This line closes the connection to the command line interface.
+	reader.close()
+
+});
