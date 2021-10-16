63256c581b099b8fca4752dd9aa7b1a53eb57acd
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-16 17:57:30 -0500

    added check password length

--- password.js
+++ password.js
@@ -9,7 +9,12 @@ const reader = readline.createInterface({
 reader.setPrompt("Welcome. Please enter your password.");
 reader.prompt();
 reader.on('line', (input) => {
-    
+   if(input.length >= 10 ) {
+       console.log("Success!");
+   } else {
+       console.log("Password is too weak. Try again.");
+   }
+   reader.close();
 });
 
 reader.question("What would you like to calculate?", function(input){
