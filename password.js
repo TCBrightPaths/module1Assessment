4dc281cb242615ad5a720554f7deea5f69fa7204
Tamara Chance <tamara.n.chance@gmail.com>
2021-10-17 10:21:04 -0500

    completed going further challenge by adding two conditions

--- password.js
+++ password.js
@@ -9,12 +9,17 @@ const reader = readline.createInterface({
 reader.setPrompt("Welcome. Please enter your password.");
 reader.prompt();
 reader.on('line', (input) => {
-   if(input.length >= 10 ) {
-       console.log("Success!");
-   } else {
-       console.log("Password is too weak. Try again.");
-   }
-   reader.close();
+    if(input.length >= 10) {
+        let re = /[0-9]/g;
+        if(input.match(re)) {
+            console.log("Success!");
+        } else {
+            console.log("password must contain at least 1 number");
+        }
+    } else if(input.length < 10) {
+        console.log("password must be at least 10 characters long and contain at least 1 number");
+    }
+  reader.close();
 });
 
 //
